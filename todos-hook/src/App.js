82dda126880e16e9a71be7183e612e8b8f14bdf5
at-trangpham2 <trang.pham2@asiantech.vn>
2020-04-10 15:26:28 +0700

    litle change

--- todos-hook/src/App.js
+++ todos-hook/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import {Header} from './components/header';
 import {InputItem} from './components/addToList';
